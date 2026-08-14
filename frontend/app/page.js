355cc4a5f7563ab74624d1175e113fc7d0e6b313
Your Name <geotangk@gmail.com>
2026-08-14 11:49:41 +0700

    Scaffold Next.js frontend with App Router strucutre

---
+++ frontend/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Movie Ticket Booking</h1>
+      <p>Welcome - Movie listing will go </p>
+    </main>
+  )
+}
